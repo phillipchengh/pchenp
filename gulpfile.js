6850f92b4c41c793b2eb2351f69cc7b183dae5e1
Phillip <phillip.chen@hotmail.com>
2014-10-15 21:05:31 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+
+var concat = require('gulp-concat');
+var debug = require('gulp-debug');
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var util = require('gulp-util');
+
+var onError = function(err) {
+  util.beep();
+  console.log(err);
+};
+
+var STYLES_SRC = [
+  'static/less/reset.less',
+  'static/less/style.less'
+];
+
+var SCRIPTS_SRC = [
+  'static/js/**'
+];
+
+var WATCH = {
+  LESS: 'static/less/**',
+  JS: 'static/js/**'
+};
+
+gulp.task('styles', function() {
+  gulp.src(STYLES_SRC)
+  .pipe(plumber({
+    errorHandler: onError
+  }))
+  .pipe(less({
+    paths: ['static/less']
+  }))
+  .pipe(concat('style.css'))
+  .pipe(gulp.dest('static/'));
+});
+
+gulp.task('scripts', function() {
+  gulp.src(SCRIPTS_SRC)
+  .pipe(plumber({
+    errorHandler: onError
+  }))
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest('static/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(WATCH.LESS, ['styles']); 
+  gulp.watch(WATCH.JS, ['scripts']);
+});
+
+gulp.task('default', ['watch', 'styles', 'scripts']);
+
+gulp.task('auto-reload', function() {
+  var process;
+  function restart() {
+    if (process) {
+      process.kill();
+    }
+
+    process = spawn('gulp', ['default'], {stdio: 'inherit'});
+  }
+
+  gulp.watch('gulpfile.js', restart);
+  restart();
+});
---
+++ pchenp.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 1337;
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+app.listen(port);
